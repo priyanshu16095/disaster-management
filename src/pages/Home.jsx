3bc0dedc68f4d389d0483fe4c57b59165a728006
Priyanshu Gupta <priyanshu16095@gmail.com>
2025-09-15 16:32:50 +0530

    Make font bold in auth page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div className='flex-v gap padding'>
-      <p>Jasleen Kaur [Leader]</p>
-      <p>Sameer Singh Sisodia</p>
-      <p>Supriya</p>
-      <p>Mahi Gautam</p>
-      <p>Sahil Singh</p>
-      <p>Priyanshu Gupta</p>
+      
     </div>
   )
 }
